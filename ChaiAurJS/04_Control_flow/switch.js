e388580b234ebeae1258b2e3215e6ea1c7cc5103
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2024-05-20 23:42:57 +0530

    arrays

---
+++ ChaiAurJS/04_Control_flow/switch.js
@@ -0,0 +1,27 @@
+// hello
+// hello
+// hello
+// hello
+// hello // shift option downarrow
+
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
